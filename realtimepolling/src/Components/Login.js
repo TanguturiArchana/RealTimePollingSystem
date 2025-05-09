5276812e2c08ba0c41a0cf3f6b3f78780a825eca
TanguturiArchana <150064169+TanguturiArchana@users.noreply.github.com>
2025-05-09 17:47:15 +0530

    Updated Login.js!

--- realtimepolling/src/Components/Login.js
+++ realtimepolling/src/Components/Login.js
@@ -13,7 +13,7 @@ function Login() {
     const navigate = useNavigate();
     const handleLogin = async () => {
         try {
-            const res = await fetch("http://localhost:5000/api/auth/login", {
+            const res = await fetch("https://realtimepollingsystembackend.onrender.com/api/auth/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
